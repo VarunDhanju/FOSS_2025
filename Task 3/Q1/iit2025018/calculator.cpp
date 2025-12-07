a2e8bf385101a691787dca2586fd8ad3c54f7311
Varunveer Singh Dhanju <varunveersinghdhanju@Varunveers-MacBook-Air.local>
2025-12-07 18:36:47 +0530

    code calculator

---
+++ Task 3/Q1/iit2025018/calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int x,y;
+	cout<<"enter the numbers:";
+	char a;
+	cin>>a;
+	if(a=='+'){
+		cout<<x+y;
+		}
+
+	else if(a=='-'){
+
+		cout<<x-y;
+		}
+	else if(a=='*'){
+		cout<<x*y;
+	}
+	else {
+		cout<<x/y;
+		}
+	}
